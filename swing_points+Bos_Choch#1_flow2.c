db9ad6b14ca53f913d3761877273a5ebcef7c46a
corncordaine <geoffreywicklife80@gmail.com>
2026-02-17 21:13:06 +0300

    Use of additional if statement

---
+++ swing_points+Bos_Choch#1_flow2.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+//quantifiers , functions, variables loops//
+
+int swing_high(float candle[], int i , int size){
+    if(i<=0 || i >=size-1){
+        return 0;
+    }
+
+    if (candle[i]>candle[i-1] && candle[i]>candle[i+1] && candle[i] >= candle [i-1]+2 && candle[i] >= candle [i+1]+2 )
+    {
+        return 1;
+    }
+    return 0;
+}
+int swing_low(float candle[], int i , int size){
+    if(i<=0 || i >=size-1){
+        return 0;
+    }
+
+    if (candle[i]<candle[i-1] && candle[i]<candle[i+1])
+    {
+        return 1;
+    }
+    return 0;
+}
+int size_of_the_candle(int size){
+printf("cardinality of the data set is %d", size);
+return size;
+
+}
+int main(){
+float candles[] ={13.3, 10.5, 18.4, 15.5, 20.6, 17.8, 16.4};
+
+int size = size_of_the_candle(sizeof(candles)/sizeof(candles[0]));
+
+for(int i = 0; i < size; i++){
+
+   if (swing_high(candles,i,size)){
+    printf("\nThe swing high in the data set is %f and it is located at the %d location", candles[i],i);
+   }
+   if(swing_low(candles,i,size)){
+    printf("\nThe swing low in the data set is %f and it is located at the %d location", candles[i],i);
+   }
+}
+
+
+//CHOCH BREAK OF STRUCTURE//
+
+
+float highest_point = 0;
+
+for (int i =0;i < size; i++){
+
+if(highest_point > 0 && candles[i] > highest_point){
+    printf("\n--- BOS ALERT! Price %.2f broke above previous high %.2f ---", candles[i], highest_point);
+     highest_point = candles[i]; 
+    
+}
+if(swing_high(candles,i,size)){
+    highest_point = candles[i];
+    printf("\nnew resistance of %.2f found", highest_point);
+}
+
+
+}
+    
+return 0;
+}    
+
+    
